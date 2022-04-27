af3cf2a6229cc1e6fba97ed25ba4cbc0cd408797
Ilya Kolebayev <kolebayev@gmail.com>
2022-04-27 17:25:11 +0300

    Add font loading response

--- code.js
+++ code.js
@@ -5,55 +5,57 @@ Promise.all([
   figma.loadFontAsync({ family: 'Inter', style: 'Regular' }),
   figma.loadFontAsync({ family: 'Inter', style: 'Semi Bold' }),
   figma.loadFontAsync({ family: 'Inter', style: 'Bold' }),
-  //   figma.loadFontAsync({ family: '12313', style: 'Bold' }),
-]).catch((err) => console.log(err));
+
+  figma.loadFontAsync({ family: 'Segoe UI', style: 'Light' }),
+  figma.loadFontAsync({ family: 'Segoe UI', style: 'Regular' }),
+  figma.loadFontAsync({ family: 'Segoe UI', style: 'Semibold' }),
+  figma.loadFontAsync({ family: 'Segoe UI', style: 'Bold' }),
+
+])
+.then(() => {
+  figma.ui.postMessage('fonts-loaded');
+
+})
+.catch((err) => {
+  console.log(err)
+  figma.ui.postMessage('font load error');
+});
 
 figma.ui.onmessage = (msg) => {
-  if (msg.type === 'create-rectangles') {
+  if (msg.type === 'create') {
     let s2 = figma.currentPage.findAll((n) => n.type === 'TEXT');
 
-    // console.log(s2);
-    // console.log(s2.length);
-
     s2.forEach((item) => {
-      //   console.log(typeof item.fontName);
-      // console.log(item.fontName.style);
       if (item.type === 'TEXT') {
-        // if (typeof item.fontName === 'object') {
-        //   if (item.fontName.family === 'Segoe UI') {
-        //     item.fontName = {
-        //       family: 'Inter',
-        //       style:
-        //         item.fontName.style === 'Semibold'
-        //           ? 'Semi Bold'
-        //           : item.fontName.style,
-        //     };
-        //   }
-        // }
+        
+        if (typeof item.fontName === 'object') {
+          if (item.fontName.family === 'Segoe UI') {
+            item.fontName = {
+              family: 'Inter',
+              style:
+                item.fontName.style === 'Semibold'
+                  ? 'Semi Bold'
+                  : item.fontName.style,
+            };
+          }
+        }
 
         if (typeof item.fontName === 'symbol') {
-          console.log(
-            'before',
-            typeof item.getStyledTextSegments(['fontName'])
-          );
-          //   console.log('before', item);
 
-          item.getStyledTextSegments(['fontName']).forEach((segment) => {
-            segment.setRangeFontName(segment.start, segment.end, {
+
+          let segments = item.getStyledTextSegments(['fontName'])
+
+
+          segments.forEach((segment) => {
+            item.setRangeFontName(segment.start, segment.end, {
               family: 'Inter',
               style:
                 segment.fontName.style === 'Semibold'
                   ? 'Semi Bold'
-                  : segment.fontName.style,
+                  : segment.fontName.style + '',
             });
 
-            // item.fontName = {
-            //   family: 'Inter',
-            //   style:
-            //     item.fontName.style === 'Semibold'
-            //       ? 'Semi Bold'
-            //       : item.fontName.style,
-            // };
+
           });
 
           console.log(item.getStyledTextSegments(['fontName']));
