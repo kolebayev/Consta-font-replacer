c256a84f4f0e0ba80dde028ed1a4aac6d833e232
Ilya Kolebayev <kolebayev@gmail.com>
2022-05-11 19:18:44 +0300

    update UI

--- src/plugin.js
+++ src/plugin.js
@@ -2,68 +2,109 @@ figma.showUI(__html__);
 figma.ui.resize(400, 600);
 
 Promise.all([
-  figma.loadFontAsync({ family: 'Inter', style: 'Light' }),
-  figma.loadFontAsync({ family: 'Inter', style: 'Regular' }),
-  figma.loadFontAsync({ family: 'Inter', style: 'Semi Bold' }),
-  figma.loadFontAsync({ family: 'Inter', style: 'Bold' }),
+  figma.loadFontAsync({ family: "Inter", style: "Light" }),
+  figma.loadFontAsync({ family: "Inter", style: "Regular" }),
+  figma.loadFontAsync({ family: "Inter", style: "Semi Bold" }),
+  figma.loadFontAsync({ family: "Inter", style: "Bold" }),
 
-  figma.loadFontAsync({ family: 'Segoe UI', style: 'Light' }),
-  figma.loadFontAsync({ family: 'Segoe UI', style: 'Regular' }),
-  figma.loadFontAsync({ family: 'Segoe UI', style: 'Semibold' }),
-  figma.loadFontAsync({ family: 'Segoe UI', style: 'Bold' }),
+  figma.loadFontAsync({ family: "Segoe UI", style: "Light" }),
+  figma.loadFontAsync({ family: "Segoe UI", style: "Regular" }),
+  figma.loadFontAsync({ family: "Segoe UI", style: "Semibold" }),
+  figma.loadFontAsync({ family: "Segoe UI", style: "Bold" }),
 
   // figma.loadFontAsync({ family: 'Sego', style: 'Bold' }),
   // figma.loadFontAsync({ family: 'Sego PP', style: 'Bold' }),
 ])
   .then((res) => {
-    console.log('res', res);
-    figma.ui.postMessage('fonts-loaded');
+    console.log("res", res);
+    figma.ui.postMessage({
+      name: "fonts-loaded",
+      body: "Все нужные шрифты есть, можно запускать",
+    });
   })
   .catch((err) => {
-    console.log('err', err);
-    figma.ui.postMessage('font load error');
+    console.log("err", err);
+    figma.ui.postMessage({
+      name: "font load error",
+      body: `Не хватает ${[...err.split('"')][1]}`,
+    });
   });
 
+  let m = 0
+
 figma.ui.onmessage = (msg) => {
-  if (msg.type === 'create') {
-    console.log(123);
-    let s2 = figma.currentPage.findAll((n) => n.type === 'TEXT');
+  if (msg.type === "create") {
+    // console.log('figma executed')
 
-    console.log(`LAYERS TOTAL: ${s2.length}`);
-    figma.ui.postMessage(`layers-found:${s2.length}`);
-
-    // s2.forEach((item, idx) => {
-    //   console.log(idx)
-    //   if (item.type === 'TEXT') {
-
-    //     if (typeof item.fontName === 'object') {
-    //       if (item.fontName.family === 'Segoe UI') {
-    //         item.fontName = {
-    //           family: 'Inter',
-    //           style:
-    //             item.fontName.style === 'Semibold'
-    //               ? 'Semi Bold'
-    //               : item.fontName.style,
-    //         };
-    //       }
-    //     }
-
-    //     if (typeof item.fontName === 'symbol') {
-    //       let segments = item.getStyledTextSegments(['fontName'])
-
-    //       segments.forEach((segment) => {
-    //         item.setRangeFontName(segment.start, segment.end, {
-    //           family: 'Inter',
-    //           style:
-    //             segment.fontName.style === 'Semibold'
-    //               ? 'Semi Bold'
-    //               : segment.fontName.style + '',
-    //         });
-    //       });
-    //     }
-    //   }
+    const p1 = new Promise((resolve,reject) => {
+      figma.ui.postMessage(`work-started`);
+      console.log('p1')
+      resolve('ok')
+    })
+    // const p2 = figma.ui.postMessage({
+    //   name: "work-finished",
+    //   body: `Заменили ${s2.length} текстовых слоя(-ев) на странице ${figma.currentPage.name}`,
     // });
+
+    const p2 = new Promise((resolve, reject) => {
+      console.log('p2 start')
+      let s2 = figma.currentPage.findAll((n) => n.type === "TEXT");
+      // let s2 = [, , , , , , , , , , ,];
+      
+
+      s2.forEach((item, idx) => {
+        m = m + idx
+      })
+      resolve(s2)
+      console.log('p2 end')
+    });
+
+    Promise.all([p1, p2]).then(values => {
+      console.log(values);
+      figma.ui.postMessage({
+        name: "work-finished",
+        body: `Заменили ${values[1].length} текстовых слоя(-ев) на странице ${figma.currentPage.name}`,
+      });
+    });
+
   }
+
+  // console.log(`LAYERS TOTAL: ${s2.length}`);
+  // figma.ui.postMessage(`layers-found:${s2.length}`);
+
+  // s2.forEach(async (item, idx) => {
+
+  //   if (item.type === 'TEXT') {
+
+  //     if (typeof item.fontName === 'object') {
+  //       if (item.fontName.family === 'Segoe UI') {
+  //         item.fontName = {
+  //           family: 'Inter',
+  //           style:
+  //             item.fontName.style === 'Semibold'
+  //               ? 'Semi Bold'
+  //               : item.fontName.style,
+  //         };
+  //       }
+  //     }
+
+  //     if (typeof item.fontName === 'symbol') {
+  //       let segments = item.getStyledTextSegments(['fontName'])
+
+  //       segments.forEach((segment) => {
+  //         item.setRangeFontName(segment.start, segment.end, {
+  //           family: 'Inter',
+  //           style:
+  //             segment.fontName.style === 'Semibold'
+  //               ? 'Semi Bold'
+  //               : segment.fontName.style + '',
+  //         });
+  //       });
+  //     }
+
+  //     idx === s2.length && console.log('==== READY ====')
+  //   }
+  // });
 
   //   figma.closePlugin();
 };
--- src/ui.js
+++ src/ui.js
@@ -1,20 +1,52 @@
-document.getElementById("create").onclick = () => {
-  parent.postMessage({ pluginMessage: { type: "create" } }, "*");
-  console.log('clicked')
-};
+console.clear();
 
 const fontLoadStatusNode = document.getElementById("message");
+const loader = document.getElementById("loader");
+const CTAbutton = document.getElementById("create");
+
+async function loadingState() {
+  //   CTAbutton.classList.add("Button_disabled");
+  //   console.log("clicked");
+  //   fontLoadStatusNode.textContent = "";
+  //   loader.style.display = "block";
+  await parent.postMessage({ pluginMessage: { type: "create" } }, "*");
+}
+
+CTAbutton.addEventListener("click", function () {
+  // this.classList.add("Button_disabled");
+  // console.log("button clicked");
+  // fontLoadStatusNode.textContent = "";
+  // loader.style.display = "block";
+  // setTimeout(loadingState(), 1000);
+  // loadingState()
+  parent.postMessage({ pluginMessage: { type: "create" } }, "*");
+});
 
 onmessage = (e) => {
-  if (e.data.pluginMessage === "font load error") {
-    fontLoadStatusNode.textContent = "шрифты не загружены";
+  if (e.data.pluginMessage.name === "font load error") {
+    fontLoadStatusNode.textContent = e.data.pluginMessage.body;
+    fontLoadStatusNode.classList.toggle("Text_view_alert");
     console.log("got this from the plugin code:", e.data.pluginMessage);
   }
-  if (e.data.pluginMessage === "fonts-loaded") {
-    fontLoadStatusNode.textContent = "шрифты загружены";
+  if (e.data.pluginMessage.name === "fonts-loaded") {
+    fontLoadStatusNode.textContent = e.data.pluginMessage.body;
+    fontLoadStatusNode.classList.toggle("Text_view_success");
     console.log("got this from the plugin code:", e.data.pluginMessage);
   }
-  if (e.data.pluginMessage.includes('layers-found')) {
-    fontLoadStatusNode.textContent = fontLoadStatusNode.textContent + [...e.data.pluginMessage.split(':')][1];
+  // if (e.data.pluginMessage.includes('layers-found')) {
+  //   fontLoadStatusNode.textContent = fontLoadStatusNode.textContent + [...e.data.pluginMessage.split(':')][1];
+  // }
+  if (e.data.pluginMessage === "work-started") {
+    console.log('work-started')
+    CTAbutton.classList.add("Button_disabled");
+    fontLoadStatusNode.textContent = "";
+    loader.style.display = "block";
+  }
+  if (e.data.pluginMessage.name === "work-finished") {
+    console.log('work-finished')
+    loader.style.display = "none";
+    CTAbutton.classList.remove("Button_disabled");
+    fontLoadStatusNode.classList.toggle("Text_view_success");
+    fontLoadStatusNode.textContent = e.data.pluginMessage.body;
   }
 };
